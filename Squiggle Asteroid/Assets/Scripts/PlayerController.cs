7989ea87d73938a520fd44fb24413f22ea5df9d1
George W J <gheathcollins@gmail.com>
2018-12-03 13:11:46 +0000

    initial project commit

---
+++ Squiggle Asteroid/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+
+    private Rigidbody2D rb2d;
+
+    float moveHorizontal;
+
+    void Start () {
+
+        rb2d = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+
+    private void Update()
+    {
+        
+    }
+
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal") * speed;
+
+    }
+}
