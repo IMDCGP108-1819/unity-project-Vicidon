c66b130377fa7e3a3d9dcb43a5482df01f9fed1e
Viktor <44615190+Vicidon@users.noreply.github.com>
2018-12-30 19:41:04 +0200

    movement commit

--- Squiggle Asteroid/Assets/Scripts/player.cs
+++ Squiggle Asteroid/Assets/Scripts/player.cs
@@ -2,40 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
-
-    public float HorizontalMovement;
-    public Rigidbody2D rb2d;
-
+public class player : MonoBehaviour
+{
+    private Rigidbody2D rb2d;
     [SerializeField]
     private float speed;
+    
+    // Start is called before the first frame update
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
     }
 
-    private void LateUpdate()
-    {
-        if (transform.position.x > 5.7)
-        {
-            transform.position = new Vector3(5.7f, transform.position.y);
-        }
-
-        if (transform.position.x <-5.7)
-        {
-            transform.position = new Vector3(-5.7f, transform.position.y);
-        }
-    }
-
+    // Update is called once per frame
     void FixedUpdate()
     {
         float horizontal = Input.GetAxis("Horizontal");
-
         HandleMovement(horizontal);
     }
-    
+
     private void HandleMovement(float horizontal)
     {
         rb2d.velocity = new Vector2(horizontal * speed, rb2d.velocity.y);
     }
 }
+
