d6649be2542c2f87fba8b7936319e1a76939008c
Viktor <44615190+Vicidon@users.noreply.github.com>
2019-01-02 16:15:42 +0200

    Spaceship is now shooting

---
+++ Squiggle Asteroid/Assets/Scripts/LaserBullet.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserBullet : MonoBehaviour
+{
+    public float MovementSpeed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x,
+            transform.position.y + MovementSpeed * Time.deltaTime);
+
+        if (transform.position.y > 5.5f)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Squiggle Asteroid/Assets/Scripts/ShpaceshipShooting.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShpaceshipShooting : MonoBehaviour
+{
+    public GameObject BulletPrefab;
+    public GameObject[] BulletPool;
+    public int MaxBullets;
+
+    // Use this for initialization
+    void Start()
+    {
+        BulletPool = new GameObject[MaxBullets];
+
+        for (int i = 0; i < MaxBullets; i++)
+        {
+            BulletPool[i] = Instantiate(BulletPrefab);
+            BulletPool[i].SetActive(false);
+        }
+    }
+
+    private GameObject GetSpareBullet()
+    {
+        for (int i = 0; i < MaxBullets; i++)
+        {
+            if (BulletPool[i].activeSelf == false)
+            {
+                return BulletPool[i];
+            }
+        }
+
+        return null;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            GameObject bullet = GetSpareBullet();
+
+            if (bullet != null)
+            {
+                bullet.SetActive(true);
+                bullet.transform.position = transform.position;
+            }
+        }
+    }
+}
